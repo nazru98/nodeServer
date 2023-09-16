0582dc8c1abb3cd15ca98b2b8ccca2a80c5e0d18
Nazruddin Ahmad <nazruddin.ahmad48@gmail.com>
2023-09-16 10:14:04 +0530

    Mobile server upladed

--- index.js
+++ index.js
@@ -161,3 +161,151 @@ app.get('/employees', (req, res) => {
      }
     });
   });
+
+  app.get('/mobiles', (req, res) => {
+    let RAM = req.query.RAM;
+    let ROM = req.query.ROM;
+    let brand = req.query.brand;
+  
+    let sql = 'SELECT * FROM mobiles WHERE 1=1';
+    let params = [];
+  
+    if (RAM && RAM.length > 0) {
+      const ramValues = RAM.split(','); 
+      sql += ` AND RAM IN (${ramValues.map((_, index) => `$${index + 1}`).join(', ')})`;
+      params.push(...ramValues);
+    }
+  
+    if (ROM && ROM.length > 0) {
+      const romValues = ROM.split(','); 
+      sql += ` AND ROM IN (${romValues.map((_, index) => `$${params.length + index + 1}`).join(', ')})`;
+      params.push(...romValues);
+    }
+  
+    
+    if (brand && brand.length > 0) {
+      const brandValues = brand.split(','); 
+      sql += ` AND brand IN (${brandValues.map((_, index) => `$${params.length + index + 1}`).join(', ')})`;
+      params.push(...brandValues);
+    }
+  
+    client.query(sql, params, (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(data.rows);
+      }
+    });
+  });
+  
+    
+  
+    
+    app.get('/mobiles/:id', (req, res) => {
+      let id = +req.params.id;
+    
+      const sql = 'SELECT * FROM mobiles where id=$1';
+      const params = [id];
+    
+      client.query(sql, params, (err, data) => {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.send(data.rows);
+        }
+      });
+    });
+    
+    app.get('/mobiles/RAM/:RAM', (req, res) => {
+      let RAM=req.params.RAM
+      const sql = 'SELECT * FROM mobiles where RAM=$1';
+      const params = [RAM];
+      client.query(sql,params, (err, data) => {
+        if (err)  res.status(404).send( err);
+       else{
+        res.send( data.rows );
+       }
+      });
+    });
+    
+   
+  
+    app.get('/mobiles/ROM/:ROM', (req, res) => {
+      let ROM=req.params.ROM
+      const sql = 'SELECT * FROM mobiles where ROM=$1';
+      const params = [ROM];
+      client.query(sql,params, (err, data) => {
+        if (err)  res.status(404).send( err);
+       else{
+        res.send( data.rows );
+       };
+      });
+    });
+    
+  
+    app.get('/mobiles/OS/:os', (req, res) => {
+      let os=req.params.os
+      const sql = 'SELECT * FROM mobiles where os=$1';
+      const params = [os];
+      client.query(sql,params, (err, data) => {
+        if (err)  res.status(404).send( err);
+       else{
+        res.send( data.rows );
+       }
+      });
+    });
+    
+  
+    app.get('/mobiles/brand/:brand', (req, res) => {
+      let brandArr=req.params.brand
+      const sql = 'SELECT * FROM mobiles where brand=$1';
+      const params = [brandArr];
+      client.query(sql,params, (err, data) => {
+        if (err)  res.status(404).send( err);
+       else{
+        res.send( data.rows );
+       }
+      });
+    });
+    
+    app.post('/mobiles', (req, res) => {
+      const body = req.body;
+      const sql = 'INSERT INTO mobiles ( name, price, brand, RAM, ROM,OS) VALUES ($1, $2, $3, $4, $5, $6 )';
+      const values = [ body.name, body.price, body.brand, body.ram, body.rom,body.os];
+    
+      client.query(sql, values, (err, data) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(201).json({ message: 'Employee added successfully', data: data.rows });
+        }
+      });
+    });
+  
+    app.put('/mobiles/:id', (req, res) => {
+      let id = +req.params.id;
+      let body = req.body;
+      const sql = 'UPDATE mobiles SET name=$1,price=$2, brand=$3, RAM=$4, ROM=$5, OS=$6 WHERE id=$7';
+    
+      const params = [body.name, body.price, body.brand, body.ram, body.rom, body.os, id];
+    
+      client.query(sql, params, (err, data) => {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.send(data.rows);
+        }
+      });
+    });
+  
+    app.delete('/mobiles/:id', (req, res) => {
+      let id=+req.params.id
+      const sql = 'delete from mobiles where id=$1';
+      const params = [id];
+      client.query(sql,params, (err, data) => {
+        if (err)  res.status(404).send( err);
+       else{
+        res.send( data );
+       }
+      });
+    });
